7135f7c83430b3368ca4f3def934d6d45e75cbdd
makadamia0055 <makend91@naver.com>
2024-05-10 15:15:11 +0900

    Spring web mvc Todo setup(directory structure, files..)

--- src/main/java/org/zerock/springex/sample/EventSampleDAOImpl.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.springex.sample;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Repository;
-
-@Qualifier("event")
-@Repository
-public class EventSampleDAOImpl implements SampleDAO{
-}
